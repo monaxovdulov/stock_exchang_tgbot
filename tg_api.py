678b803b989c729ec672ed1f01356e86995906b4
fovellixxx <ivanchernenkov93@gmail.com>
2025-10-07 19:28:53 +0300

    Добавили рабочий getUpdates

--- tg_api.py
+++ tg_api.py
@@ -2,7 +2,6 @@
 
 from typing import Any
 import requests
-
 def get_updates(
     session: requests.Session,
     base: str,
@@ -13,7 +12,15 @@ def get_updates(
     Вызывает getUpdates. Если offset=None — заберём все накопившиеся апдейты (до лимита).
     timeout_s — сервер будет держать соединение до N секунд (long-poll).
     """
-    
+    params={
+        'timeout':timeout_s,
+        'offset':offset
+    }
+    response=session.get(base + '/getUpdates', params=params, timeout=(5, timeout_s+5))
+    response.raise_for_status()
+    data=response.json()
+    return data['result']
+
     
 
 def send_message(
